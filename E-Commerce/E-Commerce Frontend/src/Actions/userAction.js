cca803cf1862b6e70bb61d8b6f4c4fc58d8c3b0f
AikaMinami <aikatan.aikachan@gmail.com>
2022-06-28 00:01:48 +0700

    deleting user account

--- E-Commerce/E-Commerce Frontend/src/Actions/userAction.js
+++ E-Commerce/E-Commerce Frontend/src/Actions/userAction.js
@@ -27,3 +27,37 @@ export const getuserinfo = (email) => {
     }
     }
 }
+
+export const deleteAccount = () =>{
+    return dispatch =>{
+        dispatch({
+            type: 'USER_DELETE',
+            status: 'DELETE_LOADING',
+            loading: true
+        })
+        const userRef = firebaseApp.database().ref(`users/${firebaseApp.auth().currentUser.email.replace('.', '_')}`)
+        const cartRef = firebaseApp.database().ref(`carts/${firebaseApp.auth().currentUser.email.replace('.', '_')}`)
+        firebaseApp.auth().currentUser.delete().then(()=>{
+            userRef.remove().then(val=>{
+                cartRef.remove()
+                dispatch({
+                    type: 'USER_DELETE',
+                    status: 'DELETE_SUCCESS',
+                    loading: false
+                })
+            }, err=>[
+                dispatch({
+                    type: 'USER_DELETE',
+                    status: 'DELETE_FAILED',
+                    loading: false
+                })
+            ])
+        }, err =>{
+            dispatch({
+                type: 'USER_DELETE',
+                status: 'DELETE_FAILED',
+                loading: false
+            })
+        })
+    }
+}
