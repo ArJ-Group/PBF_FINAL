d693dcd5e89c3fb7f673ffb72663f4e419c26dca
ArionoS <arionoseptian0802@example.com>
2022-06-28 05:17:08 +0700

    input

--- E-Commerce/E-Commerce Backend/src/contents/Users.js
+++ E-Commerce/E-Commerce Backend/src/contents/Users.js
@@ -21,4 +21,31 @@ const UserComponent = (props) => {
     }
   }, [props.user]);
 
+  const updateUser = (user) => {
+    props.updateUser(user);
+  };
+  const showUpdate = (item) => {
+    setShowUpdateModal(true);
+    setUserUpdate({
+      id: item[0],
+      ...item[1],
+    });
+  };
+  const closeUpdate = () => {
+    setShowUpdateModal(false);
+  };
+  const onSearch = (input) => {
+    if(input.target.value){
+      let searchedData = Object.entries(props.user.users).filter(user=>{
+        const emailFound =  user[0].match(input.target.value)
+        const nameFound =  user[1].name.match(input.target.value)
+        const phoneFound =  user[1].phone.match(input.target.value)
+        const addressFound =  user[1].address.match(input.target.value)
+        return emailFound || phoneFound || nameFound || addressFound
+      })
+      setUsers(searchedData)
+    }else{
+      setUsers(Object.entries(props.user.users))
+    }
+  };
 };
