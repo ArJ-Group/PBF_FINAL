d5d16c7db557c399d92056a23d5b72cdbe9e1889
AikaMinami <aikatan.aikachan@gmail.com>
2022-06-27 23:57:19 +0700

    register features

--- E-Commerce/E-Commerce Frontend/src/Actions/authAction.js
+++ E-Commerce/E-Commerce Frontend/src/Actions/authAction.js
@@ -35,3 +35,34 @@ export const authenticate = (username, password) =>{
             })
     }
 }
+
+export const register = (user)=>{
+    return dispatch =>{
+        FirebaseApp.auth().createUserWithEmailAndPassword(user.email, user.password).then(credential=>{
+            let body = {
+                name: user.name,
+                address: user.address,
+                phone: user.phone
+            }
+            FirebaseApp.database().ref(`/users/${user.email.replace('.', '_')}`)
+            .set(body)
+            dispatch({
+                type: 'USER_AUTH',
+                auth: true,
+                loading: false,
+                loginfailed: false,
+                user: {
+                    uname: credential.user.email
+                }
+            })
+        }, val=>{
+            dispatch({
+                type: 'USER_AUTH',
+                auth: false,
+                loading: false,
+                loginfailed: true,
+                user: null
+            })
+        })
+    }
+}
